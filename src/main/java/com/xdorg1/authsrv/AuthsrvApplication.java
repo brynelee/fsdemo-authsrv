96060351bedbfa7b777f30b999e124c9f008bb5f
Bryne Lee <bryne_lxd@sina.com>
2020-03-12 23:29:06 +0800

    initialized the project

---
+++ src/main/java/com/xdorg1/authsrv/AuthsrvApplication.java
@@ -0,0 +1,13 @@
+package com.xdorg1.authsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthsrvApplication.class, args);
+    }
+
+}
