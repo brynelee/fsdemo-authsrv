c03ed07d0bced960fc93dfc8e91fc97605be899e
Bryne Lee <bryne_lxd@sina.com>
2020-03-13 18:45:51 +0800

    initial password and client type

--- src/main/java/com/xdorg1/authsrv/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/xdorg1/authsrv/config/AuthorizationServerConfiguration.java
@@ -2,9 +2,14 @@ package com.xdorg1.authsrv.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 /*
 spring.security.oauth2.client.authorization-uri=/oauth/authorize
@@ -24,5 +29,26 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("eagleeye")
+                .secret("thisissecret")
+                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+                .scopes("webclient", "mobileclient");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+    }
 
 }
